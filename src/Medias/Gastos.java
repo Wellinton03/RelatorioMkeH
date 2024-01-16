bc40c6bb3e087329421924e3fcb672cecd2f0563
Wellinton03 <wellintonhf123@gmail.com>
2024-01-16 16:15:32 -0300

    novos comandos

--- src/Medias/Gastos.java
+++ src/Medias/Gastos.java
@@ -8,6 +8,8 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
@@ -22,6 +24,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Gastos extends JFrame {
 
 	// Relação com o problema
@@ -29,8 +36,8 @@ public class Gastos extends JFrame {
 	private JTextField cidade;
 	private JTextField dataProblema;
 	private JTextField tempSolucao;
-	private JTextField descricaoProblema;
-	private JTextField acoesCorretivas;
+	private JTextArea descricaoProblema;
+	private JTextArea acoesCorretivas;
 
 	// Km e Combustivel
 	double custoCombustivel = 5.50;
@@ -110,14 +117,16 @@ public class Gastos extends JFrame {
 			
 			JLabel acoesCorretivasTxt = new JLabel("Qual a ação foi tomada?");
 			acoesCorretivasTxt.setFont(new Font("Arial", Font.BOLD, 12));
-			gbcHk.gridx = 1;
-			gbcHk.gridy = 14;
+			gbcHk.gridx = 3;
+			gbcHk.gridy = 16;
 			panelHk.add(acoesCorretivasTxt, gbcHk);
 			
-			acoesCorretivas = new JTextField(20);
-			gbcHk.gridx = 1;
-			gbcHk.gridy = 15;
-			panelHk.add(acoesCorretivas, gbcHk);
+			 acoesCorretivas = new JTextArea(5, 20); 
+			 acoesCorretivas.setLineWrap(true);
+			 acoesCorretivas.setWrapStyleWord(true);
+			 gbcHk.gridx = 3;
+			 gbcHk.gridy = 17;
+			 panelHk.add(new JScrollPane(acoesCorretivas), gbcHk);
 			
 			JLabel descricaoProblemaTxt = new JLabel("Descreva o problema:");
 			descricaoProblemaTxt.setFont(new Font("Arial", Font.BOLD, 12));
@@ -125,20 +134,22 @@ public class Gastos extends JFrame {
 			gbcHk.gridy = 16;
 			panelHk.add(descricaoProblemaTxt, gbcHk);
 
-			descricaoProblema = new JTextField(20);
-			gbcHk.gridx = 1;
-			gbcHk.gridy = 17;
-			panelHk.add(descricaoProblema, gbcHk);
+			descricaoProblema = new JTextArea(5, 20); 
+		    descricaoProblema.setLineWrap(true);
+		    descricaoProblema.setWrapStyleWord(true);
+		    gbcHk.gridx = 1;
+		    gbcHk.gridy = 17;
+		    panelHk.add(new JScrollPane(descricaoProblema), gbcHk);
 			
-			JLabel custoMateriaPrimaTxt = new JLabel("Insira os custos da matéria prima adicional:");
+			JLabel custoMateriaPrimaTxt = new JLabel("Custos da matéria prima adicional:");
 			custoMateriaPrimaTxt.setFont(new Font("Arial", Font.BOLD, 12));
 			gbcHk.gridx = 1;
-			gbcHk.gridy = 18;
+			gbcHk.gridy = 14;
 			panelHk.add(custoMateriaPrimaTxt, gbcHk);
 
 			custoMateriaPrima = new JTextField(20);
 			gbcHk.gridx = 1;
-			gbcHk.gridy = 19;
+			gbcHk.gridy = 15;
 			panelHk.add(custoMateriaPrima, gbcHk);
 			
 
@@ -197,40 +208,81 @@ public class Gastos extends JFrame {
 			gbcHk.gridy = 15;
 			panelHk.add(hrsTrabalhadas2, gbcHk);
 
-			JLabel distanciaTxt = new JLabel("Insira a distância percorrida em quilômetros:");
+			JLabel distanciaTxt = new JLabel("Distância percorrida:");
 			distanciaTxt.setFont(new Font("Arial", Font.BOLD, 12));
-			gbcHk.gridx = 3;
-			gbcHk.gridy = 16;
+			gbcHk.gridx = 1;
+			gbcHk.gridy = 18;
 			panelHk.add(distanciaTxt, gbcHk);
 
 			distancia = new JTextField(20);
-			gbcHk.gridx = 3;
-			gbcHk.gridy = 17;
+			gbcHk.gridx = 1;
+			gbcHk.gridy = 19;
 			panelHk.add(distancia, gbcHk);
-			
-			JLabel resultadoFuncionarios = new JLabel();
-			gbcHk.gridx = 4;
+			//
+			JLabel resultadoCliente = new JLabel();
+			gbcHk.gridx = 5;
 			gbcHk.gridy = 5;
+			panelHk.add(resultadoCliente, gbcHk);
+			
+			JLabel resultadoCidade = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 6;
+			panelHk.add(resultadoCidade, gbcHk);
+			
+			JLabel resultadoDistancia = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 7;
+			panelHk.add(resultadoDistancia, gbcHk);
+			
+			JLabel resultadoDataProblema = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 8;
+			panelHk.add(resultadoDataProblema, gbcHk);
+			
+			
+			JLabel resultadoDescricaoProblema = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 9;
+			panelHk.add(resultadoDescricaoProblema, gbcHk);
+			
+			JLabel resultadoSolucaoProblema = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 10;
+			panelHk.add(resultadoSolucaoProblema, gbcHk);
+			
+			JLabel resultadoTempoDeSolucao = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 11;
+			panelHk.add(resultadoTempoDeSolucao, gbcHk);
+			
+			JLabel resultadoHorasTrabalhadas = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 12;	
+			panelHk.add(resultadoHorasTrabalhadas, gbcHk);
+			//
+			JLabel resultadoFuncionarios = new JLabel();
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 13;
 			panelHk.add(resultadoFuncionarios, gbcHk);
 			
 			JLabel resultadoGastoTotal = new JLabel();
-			gbcHk.gridx = 4;
-			gbcHk.gridy = 6;
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 14;
 			panelHk.add(resultadoGastoTotal, gbcHk);
 
 			JLabel resultadoGastosHoras = new JLabel();
-			gbcHk.gridx = 4;
-			gbcHk.gridy = 7;
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 15;
 			panelHk.add(resultadoGastosHoras, gbcHk);
 
 			JLabel resultadoGastosCombustivel = new JLabel();
-			gbcHk.gridx = 4;
-			gbcHk.gridy = 8;
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 16;
 			panelHk.add(resultadoGastosCombustivel, gbcHk);
 
 			JLabel resultadoGastosMateriaPrima = new JLabel();
-			gbcHk.gridx = 4;
-			gbcHk.gridy = 9;
+			gbcHk.gridx = 5;
+			gbcHk.gridy = 17;
 			panelHk.add(resultadoGastosMateriaPrima, gbcHk);
 			
 			JButton buttonHk = new JButton("CALCULAR");
@@ -262,42 +314,82 @@ public class Gastos extends JFrame {
 			
 			buttonReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-		        limparCamposTexto();
+				limparCamposTexto(resultadoCliente, resultadoFuncionarios, resultadoDistancia,
+		                resultadoCidade, resultadoDataProblema, resultadoDescricaoProblema,
+		                resultadoSolucaoProblema, resultadoTempoDeSolucao, resultadoHorasTrabalhadas,
+		                resultadoGastosHoras, resultadoGastosCombustivel, resultadoGastosMateriaPrima,
+		                resultadoGastoTotal);
 		    }
 			});
 			
 			buttonHk.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					try {
+
+		                
 						double hrFuncionarioDouble = Double.parseDouble(hrFuncionario.getText());
 						double hrFuncionarioDouble2 = Double.parseDouble(hrFuncionario2.getText());
 						double hrsTrabalhadasDouble = Double.parseDouble(hrsTrabalhadas.getText());
+						double hrsTrabalhadasDouble2 = Double.parseDouble(hrsTrabalhadas2.getText());
 						double distanciaDouble = Double.parseDouble(distancia.getText());
-						double hrsTrabalhadasDouble2 = Double.parseDouble(hrsTrabalhadas.getText());
 					    double custoMateriaPrimaDouble = Double.parseDouble(custoMateriaPrima.getText());
 					    DecimalFormat df = new DecimalFormat("#.##");
+					    double totalTrabalhado = horasReais( hrsTrabalhadasDouble, hrsTrabalhadasDouble2);
 					    gasolinaTlGasto = gastoCombustivel(distanciaDouble, mediaConsumo, custoCombustivel);
-					    gastoTotal = calculoTotal(consumoPorKm,custoCombustivel,distanciaDouble,mediaConsumo, custoMateriaPrimaDouble, hrFuncionarioDouble, hrFuncionarioDouble2, hrsTrabalhadasDouble, hrsTrabalhadasDouble2);
-					    String gasolinaFormatada = df.format(gasolinaTlGasto);
-					    String gastoTotalFormatado = df.format(gastoTotal);					    
-					    resultadoFuncionarios.setText("<html>O(s) funcionários(s) " + nomeFunEnvol.getText() + " percorreram uma distância total de " + distancia.getText() + "km <br> "
-					    		+ "para ir e vir da cidade de "
-					            + cidade.getText()   + " vistar o cliente " + cliente.getText() + " na data " + dataProblema.getText() + ",<br>"
-					            		+ " por causa do problema " + descricaoProblema.getText() + " <br> "
-					            				+ " a solução para este problema foi" + acoesCorretivas.getText() + " <br>"
-					            						+ " levaram" + tempSolucao.getText() + " dias e " + totalDeHoras + " horas para resolver</html>");
+					    gastoTotal = calculoTotal(distanciaDouble, consumoPorKm, custoCombustivel, mediaConsumo, custoMateriaPrimaDouble, hrFuncionarioDouble, hrFuncionarioDouble2, hrsTrabalhadasDouble, hrsTrabalhadasDouble2);					
+					    String gastoTotalFormatado = df.format(gastoTotal);
+					    String gasolinaTlFormatada = df.format(gasolinaTlGasto);
+					    String totalTrabalhadoFormatado = df.format(totalTrabalhado);
+					    
+					    
+					    resultadoCliente.setText("Cliente: " + cliente.getText());
+					    resultadoFuncionarios.setText("O(s) funcionários(s) envolvidos foram: " + nomeFunEnvol.getText());
+					    resultadoDistancia.setText("Distância percorrida foi: " + distancia.getText()+ "km");
+					    resultadoCidade.setText("A cidade de destino foi: " + cidade.getText());
+					    resultadoDataProblema.setText("Data do ocorrido: " + dataProblema.getText());
+					    resultadoDescricaoProblema.setText("Descrição do problema: " + descricaoProblema.getText());
+					    resultadoSolucaoProblema.setText("Solução: " + acoesCorretivas.getText());
+					    resultadoTempoDeSolucao.setText("Dias usados para solucionar o problema: " + tempSolucao.getText());
+					    resultadoHorasTrabalhadas.setText("Horas de trabalho pelo(s) funcionários(as): " + totalTrabalhadoFormatado + "hr(s)");				    					    								            						
 					    resultadoGastosHoras.setText("Os gastos com horas foi R$" + totalDeHoras + " reais");
-					    resultadoGastosCombustivel.setText("Os gastos com Combustível foi R$" + gasolinaFormatada + " reais");
+					    resultadoGastosCombustivel.setText("Os gastos com Combustível foi R$" + gasolinaTlFormatada + " reais");
 					    resultadoGastosMateriaPrima.setText("Os gastos com Matéria prima foi R$" + custoMateriaPrimaDouble + " reais");
 					    resultadoGastoTotal.setText("O gasto total foi de R$" + gastoTotalFormatado + " reais");
-					} catch (NumberFormatException ex) {
-						JOptionPane.showMessageDialog(Gastos.this, "Por favor, insira valores numéricos válidos.",
-								"Erro", JOptionPane.ERROR_MESSAGE);
-					
-					}
-				}
+					    
+					   
+					    String desktopPath = System.getProperty("user.home") + "/Desktop/";
+			            File outputFile = new File(desktopPath + "Relatorio.txt");
+			            
+						try (BufferedWriter writer = new BufferedWriter( new FileWriter (outputFile, true))) {
+			                writer.write("Cliente: " + cliente.getText() + "\n");
+			                writer.write("O(s) funcionários(s) envolvidos foram: " + nomeFunEnvol.getText() + "\n");
+			                writer.write("Distância percorrida foi: " + distancia.getText() + "km\n");
+			                writer.write("A cidade de destino foi: " + cidade.getText() + "\n");
+			                writer.write("Data do ocorrido: " + dataProblema.getText() + "\n");
+			                writer.write("Descrição do problema: " + descricaoProblema.getText() + "\n");
+			                writer.write("Solução: " + acoesCorretivas.getText() + "\n");
+			                writer.write("Dias usados para solucionar o problema: " + tempSolucao.getText() + "\n");
+			                writer.write("Horas de trabalho pelo(s) funcionários(as): " + totalTrabalhadoFormatado + "hr(s)\n");
+			                writer.write("Os gastos com horas foi R$" + totalDeHoras + " reais\n");
+			                writer.write("Os gastos com Combustível foi R$" + gasolinaTlFormatada + " reais\n");
+			                writer.write("Os gastos com Matéria prima foi R$" + custoMateriaPrimaDouble + " reais\n");
+			                writer.write("O gasto total foi de R$" + gastoTotalFormatado + " reais\n\n");
+			                writer.write(" \n");
+			          
+					    writer.close();
+			                JOptionPane.showMessageDialog(Gastos.this, "Relatório salvo com sucesso!",
+			                        "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+			            } catch (IOException ex) {
+			                JOptionPane.showMessageDialog(Gastos.this, "Erro ao salvar o arquivo.",
+			                        "Erro", JOptionPane.ERROR_MESSAGE);
+			            }
 
-					});
+			        } catch (NumberFormatException ex) {
+			            JOptionPane.showMessageDialog(Gastos.this, "Por favor, insira valores numéricos válidos.",
+			                    "Erro", JOptionPane.ERROR_MESSAGE);
+			        }
+			    }
+			});
 			
 
 			add(panelHk, BorderLayout.WEST);
@@ -305,7 +397,12 @@ public class Gastos extends JFrame {
 			repaint();
 	}
 
-private void limparCamposTexto() {
+private void limparCamposTexto(JLabel resultadoCliente, JLabel resultadoFuncionarios, JLabel resultadoDistancia,
+JLabel resultadoCidade, JLabel resultadoDataProblema, JLabel resultadoDescricaoProblema,
+JLabel resultadoSolucaoProblema, JLabel resultadoTempoDeSolucao, JLabel resultadoHorasTrabalhadas,
+JLabel resultadoGastosHoras, JLabel resultadoGastosCombustivel, JLabel resultadoGastosMateriaPrima,
+JLabel resultadoGastoTotal) {
+	
     cliente.setText("");
     cidade.setText("");
     dataProblema.setText("");
@@ -319,6 +416,20 @@ private void limparCamposTexto() {
     hrFuncionario2.setText("");
     hrsTrabalhadas.setText("");
     hrsTrabalhadas2.setText("");
+    
+    resultadoCliente.setText("");
+    resultadoFuncionarios.setText("");
+    resultadoDistancia.setText("");
+    resultadoCidade.setText("");
+    resultadoDataProblema.setText("");
+    resultadoDescricaoProblema.setText("");
+    resultadoSolucaoProblema.setText("");
+    resultadoTempoDeSolucao.setText("");
+    resultadoHorasTrabalhadas.setText("");
+    resultadoGastosHoras.setText("");
+    resultadoGastosCombustivel.setText("");
+    resultadoGastosMateriaPrima.setText("");
+    resultadoGastoTotal.setText("");
 }
 
 	public double calculoTotal(double distancia, double consumoPorKm, double custoCombustivel,double mediaConsumo, double custoMateriaPrima,
@@ -330,6 +441,12 @@ private void limparCamposTexto() {
 
 		return gastoTotal;
 
+	}
+	
+	public double horasReais(double hrsTrabalhadas, double hrsTrabalhadas2){
+		
+		double totalTrabalhado = hrsTrabalhadas + hrsTrabalhadas2;
+		return totalTrabalhado;
 	}
 
 	public double calculoDeHoras(double hrFuncionario, double hrsTrabalhadas, double hrFuncionario2,
